8493b51d3835f71dd3a7fe80d5a112a323115d47
Haruki <nazawaharuki@gmail.com>
2024-05-19 11:16:18 +0900

    aタグを使える形に実装

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,25 +1,44 @@
-import type { ButtonHTMLAttributes } from "react";
+import type { AnchorHTMLAttributes, ButtonHTMLAttributes } from "react";
 
-export const Button = (
-  props: ButtonHTMLAttributes<HTMLButtonElement> & {
-    color: "pink" | "orange" | "white" | "emerald" | "purple";
-  }
+type Props<T extends "button" | "a"> = (T extends "button"
+  ? ButtonHTMLAttributes<HTMLButtonElement>
+  : AnchorHTMLAttributes<HTMLAnchorElement>) & {
+  color: "pink" | "orange" | "white" | "emerald" | "purple";
+  element?: T;
+};
+
+export const Button = <T extends "button" | "a" = "button">(
+  props: Props<T>
 ) => {
+  const className = `text-center rounded-full px-12 py-2 border-2 border-black font-medium hover:brightness-90 transition-all whitespace-nowrap ${
+    props.color === "pink"
+      ? "bg-accent-pink"
+      : props.color === "orange"
+      ? "bg-accent-orange"
+      : props.color === "white"
+      ? "bg-white"
+      : props.color === "emerald"
+      ? "bg-accent-emerald"
+      : "bg-accent-purple"
+  }`;
+
+  if (props.element === "a") {
+    return (
+      <a
+        className={className}
+        {...(props as AnchorHTMLAttributes<HTMLAnchorElement>)}
+      >
+        {props.children}
+      </a>
+    );
+  }
+
+  props.type ??= "button";
+
   return (
     <button
-      className={`rounded-full px-12 py-2 border-2 border-black font-medium hover:brightness-90 transition-all whitespace-nowrap ${
-        props.color === "pink"
-          ? "bg-accent-pink"
-          : props.color === "orange"
-          ? "bg-accent-orange"
-          : props.color === "white"
-          ? "bg-white"
-          : props.color === "emerald"
-          ? "bg-accent-emerald"
-          : "bg-accent-purple"
-      }`}
-      type={props.type || "button"}
-      {...props}
+      className={className}
+      {...(props as ButtonHTMLAttributes<HTMLButtonElement>)}
     >
       {props.children}
     </button>
